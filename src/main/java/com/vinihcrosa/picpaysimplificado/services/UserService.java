6f685d3bbad92bfa42226f6cf9cdaa4dcb032279
vinicius Rosa <viniciushcrosa@gmail.com>
2023-09-02 09:20:49 -0300

    feat: create user service

---
+++ src/main/java/com/vinihcrosa/picpaysimplificado/services/UserService.java
@@ -0,0 +1,33 @@
+package com.vinihcrosa.picpaysimplificado.services;
+
+import com.vinihcrosa.picpaysimplificado.domain.user.User;
+import com.vinihcrosa.picpaysimplificado.domain.user.UserType;
+import com.vinihcrosa.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Merchant can't send money");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Insufficient funds");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return userRepository.findUSerById(id).orElseThrow(() -> new Exception("User not found"));
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+}
